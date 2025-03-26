81fcf49c8cd1466f65d3ab82a49cda8078eb5f8c
prajith <prajithraja321@gmail.com>
2025-03-27 02:02:49 +0530

    Updated login file

--- auth/login.php
+++ auth/login.php
@@ -11,7 +11,7 @@
     <div class="bg-white shadow-2xl rounded-xl p-10 max-w-md w-full text-center transform hover:scale-105 transition duration-300">
         <h2 class="text-3xl font-extrabold text-gray-800 mb-6">🔑 Login</h2>
 
-        <form action="dashboard.php" method="POST" class="space-y-4">
+        <form action="student\dashboard.php" method="POST" class="space-y-4">
             <input type="email" name="email" placeholder="Email" required class="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500">
             <input type="password" name="password" placeholder="Password" required class="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500">
             <button type="submit" class="w-full bg-green-600 text-white py-2 rounded-lg shadow-md hover:bg-green-700 hover:shadow-xl transition duration-300">Login</button>
