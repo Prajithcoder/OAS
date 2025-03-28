c4b214a336cd08b2fb4a785d15e797480d56eefb
prajith <prajithraja321@gmail.com>
2025-03-28 07:37:02 +0530

    Updated dashbord page with role selection

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-// if (!isset($_SESSION['user_id'])) {
-//     header("Location:../auth/login.php");
-//     exit();
-// }
 
 if ($_SESSION['role'] == 'student') {
     header("Location: ./student/dashboard.php");
